68a4c7034eff1f4044684a3cb14c4ea89e688fc4
Vincent Wang <vw12@rice.edu>
2026-06-01 23:46:33 -0700

    more.

--- known_gsgs/validation.py
+++ known_gsgs/validation.py
@@ -11,7 +11,7 @@ jl.include("compute_gonality.jl")
 # CHANGE THIS !!!!!!
 folder_to_vertices = {
     # "V7": [7],
-    # "V8": [8],
+    "V8": [8],
     "V9": [9],
     # "V10": [10],
     # "V11": [11],
